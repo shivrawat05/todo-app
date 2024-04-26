a6152379c540536c225e00a8ec591412cf0f8f95
shivrawat05 <shivrawat2002@gmail.com>
2024-04-26 14:35:32 +0530

    frontend

---
+++ frontend/src/components/about/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./about.css";
+const About = () => {
+  return (
+    <div className="about d-flex justify-content-center align-items-center">
+      <div className="container">
+        <div className="d-flex">
+          <h1>About Us</h1>
+        </div>
+
+        <p>
+         This is a To-Do App
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
